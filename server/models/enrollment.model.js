3b552967f20c8e63d29d3711d47aa1d7548fc952
supriyahatele <niyahatele70@gmail.com>
2024-05-11 09:53:52 +0530

    : enrollment feature added

--- server/models/enrollment.model.js
+++ server/models/enrollment.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const EnrollmentSchema = mongoose.Schema({
-    studentID: { type: [mongoose.Schema.Types.ObjectId], ref: "User" },
-    courseID: { type: [mongoose.Schema.Types.ObjectId], ref: "Course" },
+    studentID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    courseID: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
     price  : { type : Number , default: 0 },
     enrollmentDate: { type: Date, default: Date.now },
     paymentMethod:{type:"String", required : true, enum:['debit card','credit card',"upi ID"]},
