e0c1d13fb93b5630ba0a2e19fa035606812f111b
Supriya Hatele <106535101+supriyahatele@users.noreply.github.com>
2024-07-11 10:39:24 +0530

    Updated Courses.jsx component

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -183,7 +183,7 @@ function Courses() {
               <Box width={"80%"} margin={"auto"} textAlign={"center"}>
                 <Text color={'black'} fontSize={"20"} fontWeight={"medium"}>{course.courseName}</Text>
               </Box>
-              <Text color={'black'} fontWeight={"medium"} >{` Instructor ${course.educator}`}</Text>
+              <Text color={'black'} fontWeight={"medium"} >{` Instructor : ${course.educator}`}</Text>
               <Text color={'black'}>{`course fees:${course.price} ₹`}</Text>
               <Box color={'black'} height={'60px'} width={"80%"} margin={"auto"} textAlign={"center"}>
                 <Text>
